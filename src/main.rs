2187c6fba5a80eac3629944e4108cdad0e0bfe5e
Douglas Muth <dmuth@dmuth.org>
2024-10-22 20:31:41 -0400

    Added unit tests

--- src/main.rs
+++ src/main.rs
@@ -2,57 +2,51 @@
 
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
 
-use rand::Rng;
+use rand::{Rng, SeedableRng, thread_rng};
 use rand::rngs::{StdRng, ThreadRng};
 
-use rand::SeedableRng;
-use rand::thread_rng;
 
-
-fn rand_seed(seed: u64) -> StdRng {
-    StdRng::seed_from_u64(seed)
+/*
+* Our structure which holds the rng
+*/
+struct Random {
+    rng: Box<dyn rand::RngCore>,
 }
 
-fn rand_seed_no() -> ThreadRng {
-    rand::thread_rng()
+
+impl Random {
+    /*
+    * Return our seed, based on either an integer or randomly.
+    * We should really only use the integer for unit tests or other
+    * reproducable tests.
+    */
+    fn new(seed: Option<u64>) -> Self {
+
+        let rng: Box<dyn rand::RngCore> = match seed {
+            Some(s) => Box::new(StdRng::seed_from_u64(s)),
+            None => Box::new(rand::thread_rng()),
+        };
+
+        Random { rng }
+
+    }
+
+    /*
+    * Return a randon number in our range.
+    */
+    fn generate(&mut self, lower: u32, upper: u32) -> u32 {
+        self.rng.gen_range(lower..=upper)
+    }
+
 }
 
-fn generate_random_number(seed: Option<u64>) -> u32 {
-
-    //let mut rng: Box<dyn Rng> = match seed {
-    let mut rng: Box<dyn rand::RngCore> = match seed {
-        Some(s) => {
-            Box::new(rand_seed(s))
-        },
-        None => {
-            Box::new(rand_seed_no())
-
-        }
-    };
-
-    // Generate a random number between 1 and 10
-    rng.gen_range(1..=10)
-}
 
 
 fn main() {
 
-    // Create a random number generator
-    //let mut rng = rand::thread_rng();
+    let mut rng = Random::new(None);
 
-    // Generate a random integer between 1 and 10
-    //let random_number = rng.gen_range(1..=10);
-
-    //println!("Random number between 1 and 10: {}", random_number);
-    //println!("Random number between 1 and 10: {}", generate_random_number(None) );
-
-    println!("Random number between 1 and 10: {}", generate_random_number(None) );
-    println!("Random number between 1 and 10: {}", generate_random_number(None) );
-    println!("Random number between 1 and 10: {}", generate_random_number(None) );
-
-    println!("Random number between 1 and 10: {}", generate_random_number(Some(42)) );
-    println!("Random number between 1 and 10: {}", generate_random_number(Some(42)) );
-    println!("Random number between 1 and 10: {}", generate_random_number(Some(42)) );
+    println!("Random number between 1 and 10: {}", rng.generate(1, 10) );
 
     println!("Hello, world!");
 
@@ -63,18 +57,32 @@ fn main() {
 mod tests {
     use super::*;
 
-/*
     #[test]
     fn test_generate_random_number() {
-        // Run the function multiple times to test it
+
+        let mut rng = Random::new(None);
+
         for _ in 0..100 {
-            let random_number = generate_random_number(None);
-            // Check if the number is within the expected range
+            let random_number = rng.generate(1, 10);
             assert!(random_number >= 1 && random_number <= 10, 
                     "Random number out of range: {}", random_number);
         }
+
     }
-*/
+
+    #[test]
+    fn test_generate_random_number_with_seed() {
+
+        let mut rng = Random::new(Some(12345));
+
+        let random_number = rng.generate(1, 10);
+        assert_eq!(random_number, 6);
+        let random_number = rng.generate(1, 10);
+        assert_eq!(random_number, 4);
+        let random_number = rng.generate(1, 10);
+        assert_eq!(random_number, 10);
+
+    }
 
 }
 
