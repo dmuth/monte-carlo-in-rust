0bf996b64b9e101ee6366621fdbe2d008d749a08
Douglas Muth <dmuth@dmuth.org>
2024-10-24 22:27:06 -0400

    Added some helpful comments.  Helpful for me, at least.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 
 
-// Declare our random module
+//
+// Declare our random module here so that unit tests can access it, since they
+// are in another crate.
+//
 mod random;
 pub use random::Random;
 
+//
+// Declare the points module here so that other files in this crate can use it.
+//
 mod points;
+pub use points::Point;
 
